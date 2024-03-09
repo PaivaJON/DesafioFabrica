e34235a9582f792565ed28254052df6bd4865847
João Paiva <joaopaiva.oliveiraneto@gmail.com>
2024-03-09 16:08:07 -0300

    att settings e models

--- filmez/models.py
+++ filmez/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Filmes(models.Model):
+        id_filmes = models.UUIDField(primarykey = True, default = uuid4, editable = False)
+        title = models.Charfield(max_length = 300)
+        year = models.IntegerField()
+        runtime = models.IntegerField()
+        genre = models.Charfield(max_length = 300)
+        director = models.Charfield(max_length = 300)
